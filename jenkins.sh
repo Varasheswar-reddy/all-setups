94424288f74c9226921d434218e7cfd5cfda7928
Varasheswar-reddy <varasheswar428@gmail.com>
2026-07-16 19:12:17 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,11 +3,11 @@ yum install git  -y
 
 #STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+    https://pkg.jenkins.io/rpm/jenkins.repo
+sudo yum upgrade
 
 #STEP-3: DOWNLOAD JAVA11 AND JENKINS
-yum install java-17-amazon-corretto -y
+yum install java-21-amazon-corretto -y
 yum install jenkins -y
 
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
