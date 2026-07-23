ed1351fb33fb3b24785c58209375a3d7c7f9ef72
Varasheswar-reddy <varasheswar428@gmail.com>
2026-07-23 17:24:55 +0530

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,5 +1,10 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-ls /usr/local/bin/
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# Download the latest standalone binary to your local bin path
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+# Apply executable permissions
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose version
+
+# Symlink it so it is universally accessible across the system path
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
